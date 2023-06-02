e18b0f76c32cd91d6b6142ca0e2789aa08df6b5a
Mark Oates <markoates0@gmail.com>
2023-06-02 19:10:29 -0400

    * Upgrade SceneRenderer tests to fixtures

--- tests/ArtGalleryOfCats/Gameplay/SceneRendererTest.cpp
+++ tests/ArtGalleryOfCats/Gameplay/SceneRendererTest.cpp
@@ -2,9 +2,22 @@
 #include <gtest/gtest.h>
 
 #include <ArtGalleryOfCats/Gameplay/SceneRenderer.hpp>
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
 
 
-TEST(ArtGalleryOfCats_Gameplay_SceneRendererTest, can_be_created_without_blowing_up)
+class ArtGalleryOfCats_Gameplay_SceneRendererTest : public ::testing::Test {};
+class ArtGalleryOfCats_Gameplay_SceneRendererTestWithAllegroRenderingFixtureTest :
+   public AllegroFlare::Testing::WithAllegroRenderingFixture
+{};
+
+
+TEST_F(ArtGalleryOfCats_Gameplay_SceneRendererTest, can_be_created_without_blowing_up)
+{
+   ArtGalleryOfCats::Gameplay::SceneRenderer scene_renderer;
+}
+
+
+TEST_F(ArtGalleryOfCats_Gameplay_SceneRendererTest, render__renders_the_scene)
 {
    ArtGalleryOfCats::Gameplay::SceneRenderer scene_renderer;
 }
