bcb4b8860001a56c95e8fcffa3484c444364d76f
Mark Oates <markoates0@gmail.com>
2023-06-02 19:24:50 -0400

    * Add (with nothing showing) VISUAL test for SceneRenderer

--- tests/ArtGalleryOfCats/Gameplay/SceneRendererTest.cpp
+++ tests/ArtGalleryOfCats/Gameplay/SceneRendererTest.cpp
@@ -3,6 +3,8 @@
 
 #include <ArtGalleryOfCats/Gameplay/SceneRenderer.hpp>
 #include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
+#include <ArtGalleryOfCats/Gameplay/EntityFactory.hpp>
+#include <AllegroFlare/SceneGraph/EntityPool.hpp>
 
 
 class ArtGalleryOfCats_Gameplay_SceneRendererTest : public ::testing::Test {};
@@ -17,17 +19,29 @@ TEST_F(ArtGalleryOfCats_Gameplay_SceneRendererTest, can_be_created_without_blowi
 }
 
 
-TEST_F(ArtGalleryOfCats_Gameplay_SceneRendererTestWithAllegroRenderingFixtureTest, render__renders_the_scene)
+TEST_F(ArtGalleryOfCats_Gameplay_SceneRendererTestWithAllegroRenderingFixtureTest,
+   VISUAL__render__renders_the_scene)
 {
    AllegroFlare::Shaders::Cubemap cubemap_shader;
    AllegroFlare::Cubemap* cubemap = nullptr;
+   AllegroFlare::SceneGraph::EntityPool entity_pool;
+   ArtGalleryOfCats::Gameplay::EntityFactory entity_factory;
 
    cubemap_shader.initialize();
    cubemap_shader.set_cube_map(cubemap);
 
-   ArtGalleryOfCats::Gameplay::SceneRenderer scene_renderer;
+   ArtGalleryOfCats::Gameplay::Entities::Camera3D* camera = entity_factory.create_camera();
+   camera->set("primary_camera");
+   entity_pool.add(camera);
 
-   //scene_renderer.render();
+   ArtGalleryOfCats::Gameplay::SceneRenderer scene_renderer;
+   scene_renderer.set_cubemap_shader(&cubemap_shader);
+   scene_renderer.set_entity_pool(&entity_pool);
+    
+   scene_renderer.render();
+
+   al_flip_display();
+   al_rest(1);
 }
 
 
