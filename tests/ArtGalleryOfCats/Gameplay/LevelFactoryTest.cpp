8b8a7ba1cc6bbfffab255c1a657a9fdccb342e92
Mark Oates <markoates0@gmail.com>
2023-06-03 09:49:41 -0400

    * Add test to run #load_primary_map#

--- tests/ArtGalleryOfCats/Gameplay/LevelFactoryTest.cpp
+++ tests/ArtGalleryOfCats/Gameplay/LevelFactoryTest.cpp
@@ -18,3 +18,17 @@ TEST_F(ArtGalleryOfCats_Gameplay_LevelFactoryTest, can_be_created_without_blowin
 }
 
 
+TEST_F(ArtGalleryOfCats_Gameplay_LevelFactoryTestWithAllegroRenderingFixture, load_primary_map__will_not_crash)
+{
+   AllegroFlare::ModelBin model_bin;
+   AllegroFlare::SceneGraph::EntityPool entity_pool;
+   model_bin.set_full_path(get_fixtures_path() + "models");
+   ArtGalleryOfCats::Gameplay::LevelFactory level_factory;
+   level_factory.set_bitmap_bin(&get_bitmap_bin_ref());
+   level_factory.set_model_bin(&model_bin);
+   level_factory.set_entity_pool(&entity_pool);
+
+   level_factory.load_primary_map();
+}
+
+
